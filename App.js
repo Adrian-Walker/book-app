9d1bf4a2733fd391a04a9e1d5762e5dbf9211632
Adrian-Walker <awalker3365@gmail.com>
2022-06-19 22:47:06 -0400

    added text component

--- App.js
+++ App.js
@@ -5,9 +5,19 @@ export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <SafeAreaView />
-      <View style={{ height: 70, borderBottomWidth: 0.5 }} />
+      <View
+        style={{
+          height: 70,
+          borderBottomWidth: 0.5,
+          borderBottomColor: "#484",
+        }}
+      >
+        <Text>My New Books</Text>
+      </View>
       <View style={{ flex: 1 }} />
-      <View style={{ height: 70, borderBottomWidth: 0.5 }} />
+      <View
+        style={{ height: 70, borderTopWidth: 0.5, borderTopColor: "#484" }}
+      />
       <SafeAreaView />
     </View>
   );
