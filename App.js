c11debb4707a163b4120e92e88487c0ec1c88755
Adrian-Walker <awalker3365@gmail.com>
2022-06-30 10:11:36 -0400

    class

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import {
   View,
   SafeAreaView,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 import BookCount from "./components/BookCount";
+import { Ionicons } from "@expo/vector-icons";
 
 class App extends React.Component {
   constructor() {
@@ -15,60 +17,114 @@ class App extends React.Component {
       totalCount: 0,
       readingCount: 0,
       readCount: 0,
+      isAddNewBookVisible: false,
     };
   }
+
+  showAddNewBook = () => {
+    this.setState({ isAddNewBookVisible: true });
+  };
 
   render() {
     return (
       <View style={{ flex: 1 }}>
         <SafeAreaView />
+
+        {/* Header */}
+
         <View
           style={{
             height: 70,
-            borderBottomWidth: 1,
-            borderBottomColor: "#e9e9e9",
+            borderBottomWidth: 0.5,
+            borderBottomColor: "grey",
             alignItems: "center",
             justifyContent: "center",
           }}
         >
           <Text style={{ fontSize: 24 }}>Book Keeper</Text>
         </View>
-        <View style={{ flex: 1 }} />
 
-        <TouchableOpacity>
-          <View
-            style={{
-              width: 50,
-              height: 50,
-              borderRadius: 25,
-              backgroundColor: "blue",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
+        {/* Text Input */}
+        <View style={{ flex: 1 }}>
+          {this.isAddNewBookVisible && (
+            <View style={{ height: 50, flexDirection: "row" }}>
+              <TextInput
+                style={{ flex: 1, backgroundColor: "#ececec", paddingLeft: 5 }}
+                placeholder="Enter Book Title"
+                placeholderTextColor="grey"
+              />
+
+              {/* Check Button */}
+
+              <TouchableOpacity>
+                <View
+                  style={{
+                    width: 50,
+                    height: 50,
+                    backgroundColor: "green",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Ionicons name="ios-checkmark" color="white" size={40} />
+                </View>
+              </TouchableOpacity>
+
+              {/* Close Button */}
+
+              <TouchableOpacity>
+                <View
+                  style={{
+                    width: 50,
+                    height: 50,
+                    backgroundColor: "red",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Ionicons name="ios-close" color="white" size={40} />
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
+
+          {/* Plus Button */}
+          <TouchableOpacity
+            onPress={this.showAddNewBook}
+            style={{ position: "absolute", bottom: 20, right: 20 }}
           >
-            <Text
+            <View
               style={{
-                color: "white",
-                fontSize: 25,
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                backgroundColor: "blue",
+                alignItems: "center",
+                justifyContent: "center",
               }}
             >
-              +
-            </Text>
-          </View>
-        </TouchableOpacity>
+              <Text style={{ color: "white", fontSize: 40 }}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
         <View
           style={{
             height: 70,
-            borderTopWidth: 1,
-            borderTopColor: "#e9e9e9",
+            borderTopWidth: 0.5,
+            borderTopColor: "grey",
             flexDirection: "row",
           }}
         >
-          <BookCount title="Total: " count={this.state.totalCount} />
-          <BookCount title="Reading: " count={this.state.readingCount} />
-          <BookCount title="Read: " count={this.state.readCount} />
-        </View>
+          {/* Total Count */}
+          <BookCount title="Total" count={this.state.totalCount} />
 
+          {/* Reading Count */}
+          <BookCount title="Reading" count={this.state.readingCount} />
+
+          {/* Read Count */}
+          <BookCount title="Read" count={this.state.readCount} />
+        </View>
         <SafeAreaView />
       </View>
     );
@@ -76,18 +132,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#376",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  box: {
-    height: 50,
-    width: 50,
-    backgroundColor: "red",
-  },
-});
